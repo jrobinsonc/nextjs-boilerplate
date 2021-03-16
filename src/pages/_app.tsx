fc2d0f88a8130711660a0c51e55ab79b6b0c06d2
Jose Robinson <jrobinsonc@gmail.com>
2021-03-16 19:13:55 -0400

    fix: #5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import App from 'next/app';
+import Head from 'next/head'
 import '@app/styles/styles.scss';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
-    return <Component {...pageProps} />;
+    return (
+      <>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    )
   }
 }
 
