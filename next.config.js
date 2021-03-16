082ffeb20cf275898cbf26b88be8120a40a95158
Jose Robinson <jrobinsonc@gmail.com>
2021-03-16 19:12:38 -0400

    fix: #8

--- next.config.js
+++ next.config.js
@@ -4,31 +4,28 @@
  * @see https://nextjs.org/docs#usage
  */
 
-const withSass = require('@zeit/next-sass');
 const withSourceMaps = require('@zeit/next-source-maps');
 const webpackConfig = require('./webpack.config');
 const packageJson = require('./package.json');
 
 const date = new Date();
 
-module.exports = withSourceMaps(
-  withSass({
-    env: {
-      BUILD_TIME: date.toString(),
-      BUILD_TIMESTAMP: +date,
-      APP_NAME: packageJson.name,
-      APP_VERSION: packageJson.version,
-    },
+module.exports = withSourceMaps({
+  env: {
+    BUILD_TIME: date.toString(),
+    BUILD_TIMESTAMP: +date,
+    APP_NAME: packageJson.name,
+    APP_VERSION: packageJson.version,
+  },
 
-    // We can ignore Typescript issues detected by NextJS because we are
-    // using ESLint to do the validation.
-    typescript: {
-      ignoreDevErrors: false,
-      ignoreBuildErrors: false,
-    },
+  // We can ignore Typescript issues detected by NextJS because we are
+  // using ESLint to do the validation.
+  typescript: {
+    ignoreDevErrors: false,
+    ignoreBuildErrors: false,
+  },
 
-    webpack(config, options) {
-      return webpackConfig({ config, options });
-    },
-  }),
-);
+  webpack(config, options) {
+    return webpackConfig({ config, options });
+  },
+});
